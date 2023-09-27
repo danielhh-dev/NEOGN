21b26303bd60c34401fcfba1492a64cd87c0ae2b
Elias Martinez <99233887+XliazZz@users.noreply.github.com>
2023-09-27 16:36:27 -0300

    feat/back-controllers-User: add signUp

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 
-const routerProduct = require("./routerProduct");
+// const routerProduct = require("./routerProduct");
 const routerUser = require("./routerUser");
 
 const router = Router();
 
-router.use("/products", routerProduct);
+// router.use("/products", routerProduct);
 router.use("/user", routerUser);
 
 module.exports = router;
