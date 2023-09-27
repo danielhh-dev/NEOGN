a0a0611d5d9b2cd7c6b6ee33d6898898888b800d
Daniel <danielhh.dev@gmail.com>
2023-09-27 12:27:13 -0600

    routerOrder agregado

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 
 const routerProduct = require("./routerProduct");
 const routerUser = require("./routerUser");
+const routerOrders = require("./routerOrder");
 
 const router = Router();
 
 router.use("/products", routerProduct);
 router.use("/user", routerUser);
+router.use("/order", routerOrders);
 
 module.exports = router;
