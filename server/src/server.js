925acb5e5c69ba8b9cce54dbdcd13c8f1bfe1ec8
Daniel <danielhh.dev@gmail.com>
2023-09-27 19:37:57 -0600

    server actualizado

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const router = require("./routes");
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan("dev"));
 server.use(cors());
+
+server.use("/", router);
 
 module.exports = server;
