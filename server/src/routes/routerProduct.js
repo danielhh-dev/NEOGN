9816f24d3ce7741c5f6189207af70352ac17d341
Giuliano Colzani <106625526+Kbza220@users.noreply.github.com>
2023-09-29 18:40:12 -0300

    ruta delete funcional

--- server/src/routes/routerProduct.js
+++ server/src/routes/routerProduct.js
@@ -3,13 +3,13 @@ const router = Router();
 const {handlerGetProducts} = require ('../controllers/handlers/handlerGetProducts');
 const { handlerGetById } = require("../controllers/handlers/handlerGetByID");
 const {handlersCreateProducts} = require("../controllers/handlers/handlersCreateProducts");
+const {handlerDeleteById} = require ('../controllers/handlers/handlerDeleteById')
 
 
-
-router.get("/:id", handlerGetById);
-router.get("/", handlerGetProducts);
-router.post("/create", handlersCreateProducts);
-
+router.get('/:id', handlerGetById);
+router.get('/', handlerGetProducts);
+router.post('/create', handlersCreateProducts);
+router.delete('/:id', handlerDeleteById )
 
 
 
