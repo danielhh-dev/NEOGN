9494e89270cab72bfd9a9088fbcf3454082c8105
Giuliano Colzani <106625526+Kbza220@users.noreply.github.com>
2023-09-29 18:21:08 -0300

    ruta busquedabyid funcional

--- server/src/routes/routerProduct.js
+++ server/src/routes/routerProduct.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const handlersCreateProducts = require("../controllers/handlers/handlersCreateProducts");
-const {handlerGetProducts} = require ('../controllers/handlers/handlerGetProducts')
+const {handlerGetProducts} = require ('../controllers/handlers/handlerGetProducts');
+const { handlerGetById } = require("../controllers/handlers/handlerGetByID");
+const {handlersCreateProducts} = require("../controllers/handlers/handlersCreateProducts");
 
 
 
+router.get("/:id", handlerGetById);
 router.get("/", handlerGetProducts);
-
 router.post("/create", handlersCreateProducts);
 
 
