8b56244353e82451042d2eedfc7f57eb4598a40e
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2023-09-30 01:57:17 -0300

    Correcciones

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,19 +1,16 @@
 // src/components/Home.jsx
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setProducts, fetchProducts, changePage, setTotalItems } from "../redux/slices/productsSlice";
-import Pagination from "../components/Pagination"
-import productsData from "../fakeProducts.json"
-import SearchBar from "../components/SearchBar";
+import {  useSelector } from "react-redux";
+// import Pagination from "../components/Pagination"
+
 
 const Home = () => {
-  const dispatch = useDispatch();
+
   const user = useSelector((state) => state.user);
-  const products = useSelector((state) => state.products.products);
+  // const products = useSelector((state) => state.products.products);
   const status = useSelector((state) => state.products.status);
-  const currentPage = useSelector((state) => state.products.currentPage );
-  const totalItems = useSelector((state) => state.products.totalItems);
-  const itemsPerPage = 10;
+  // const currentPage = useSelector((state) => state.products.currentPage );
+  // const itemsPerPage = 10;
 
   // console.log("startIndex:", startIndex);
   // console.log("endIndex:", endIndex);
@@ -23,38 +20,27 @@ const Home = () => {
 
     if (status === "idle") {
       // Solo cargamos productos si el estado está en "idle"
-      dispatch(fetchProducts());
     }
   }, []);
 
 
-  useEffect(() => {
-  if (status === "succeeded" && productsData.length > 0) {
-    dispatch(setProducts(productsData));
-    dispatch(setTotalItems(productsData.length));
-  }
-}, [dispatch, status])
-
-
-  const handlePageChange = (newPage) => {
-    console.log("handlePageChange called with newPage:", newPage);
-    dispatch(changePage(newPage));
+  // const handlePageChange = (newPage) => {
+  //   console.log("handlePageChange called with newPage:", newPage);
     
-  };
+  // };
 
   return (
     <div className="flex flex-col items-center justify-center min-hscreen">
-      <SearchBar/>
       <h1  className="texte-3x1 font-semibold my-4 justify-center">Home</h1>
       <p>Welcome, {user.name}</p>
       <p>Email: {user.email}</p>
       
-      <Pagination
+      {/* <Pagination
         currentPage={currentPage} 
         handlePagination={handlePageChange}
         allProducts={products}
         productsPerPage={itemsPerPage}
-      />
+      /> */}
     </div>
   );
 };
