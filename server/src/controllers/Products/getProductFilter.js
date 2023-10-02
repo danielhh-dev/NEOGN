6813cf0b399b8f7c2914ed8a20d4b3c27d340b19
Giuliano Colzani <106625526+Kbza220@users.noreply.github.com>
2023-10-02 03:32:24 -0300

    nuevos filtros date

--- server/src/controllers/Products/getProductFilter.js
+++ server/src/controllers/Products/getProductFilter.js
@@ -1,10 +1,15 @@
 const db = require('../../db');
 const {calculateAverageRating} = require ('../auxfunction/calculateAverageRating');
-
+const {Op} = require ('sequelize')
 
 const getFilteredProducts = async (category, min, max, order) => {
  
-  let productsPrefilter = await db.Product.findAll();
+  let productsPrefilter = category
+    ? await db.Product.findAll({
+        where: { category: { [Op.like]: `%${category}` } },
+      })
+    : await db.Product.findAll();
+
   
   productsPrefilter = productsPrefilter.map(product =>{
     const averageRating = calculateAverageRating(product.rating);
@@ -15,11 +20,6 @@ const getFilteredProducts = async (category, min, max, order) => {
    
 
 
-
-  if (category) {
-    
-    productsPrefilter = productsPrefilter.filter((product) => product.category === category);
-  }
 
   if (min && max) {
     productsPrefilter = productsPrefilter.filter(
@@ -32,9 +32,11 @@ const getFilteredProducts = async (category, min, max, order) => {
       productsPrefilter.sort((a, b) => a.title.localeCompare(b.title));
     } else if (order === 'Z-A') {
       productsPrefilter.sort((a, b) => b.title.localeCompare(a.title));
-    } else if (order === "date") {
+    } else if (order === "Newest") {
       productsPrefilter.sort((a, b) => a.createdAt - b.createdAt);
-    } else if (order === 'rating') {
+    }else if (order === "Oldest") {
+      productsPrefilter.sort((a, b) => b.createdAt - a.createdAt);
+    }else if (order === 'rating') {
       productsPrefilter.sort((a, b) => b.averageRating - a.averageRating);
     }else if (order === "price-high") {
         productsPrefilter.sort((a, b) => b.price - a.price);
