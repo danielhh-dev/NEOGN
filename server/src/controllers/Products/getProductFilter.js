362136bde189b27d29b06e4ee98e71080945c061
Giuliano Colzani <106625526+Kbza220@users.noreply.github.com>
2023-10-02 03:03:24 -0300

    filter terminada

--- server/src/controllers/Products/getProductFilter.js
+++ server/src/controllers/Products/getProductFilter.js
@@ -3,7 +3,7 @@ const {calculateAverageRating} = require ('../auxfunction/calculateAverageRating
 
 
 const getFilteredProducts = async (category, min, max, order) => {
-  
+ 
   let productsPrefilter = await db.Product.findAll();
   
   productsPrefilter = productsPrefilter.map(product =>{
@@ -12,34 +12,38 @@ const getFilteredProducts = async (category, min, max, order) => {
     return product;
     });
 
+   
+
 
 
   if (category) {
-    products = products.filter((product) => product.category === category);
+    
+    productsPrefilter = productsPrefilter.filter((product) => product.category === category);
   }
 
   if (min && max) {
-    products = products.filter(
+    productsPrefilter = productsPrefilter.filter(
       (product) => product.price >= min && product.price <= max
     );
   }
 
   if (order) {
     if (order === 'A-Z') {
-      products.sort((a, b) => a.title.localeCompare(b.title));
+      productsPrefilter.sort((a, b) => a.title.localeCompare(b.title));
     } else if (order === 'Z-A') {
-      products.sort((a, b) => b.title.localeCompare(a.title));
+      productsPrefilter.sort((a, b) => b.title.localeCompare(a.title));
     } else if (order === "date") {
-      products.sort((a, b) => a.createdAt - b.createdAt);
+      productsPrefilter.sort((a, b) => a.createdAt - b.createdAt);
     } else if (order === 'rating') {
-      products.sort((a, b) => b.averageRating - a.averageRating);
+      productsPrefilter.sort((a, b) => b.averageRating - a.averageRating);
     }else if (order === "price-high") {
-        products.sort((a, b) => b.price - a.price);
+        productsPrefilter.sort((a, b) => b.price - a.price);
     } else if (order === "price-low") {
-        products.sort((a, b) => a.price - b.price);
+        productsPrefilter.sort((a, b) => a.price - b.price);
     } 
   } 
-  return products;
+  
+  return productsPrefilter;
 };
 
 module.exports = {getFilteredProducts};
