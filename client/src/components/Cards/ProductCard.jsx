ac4a872407d81c9c8589b7d4f202a2d8a33b71a2
Giuliano Colzani <106625526+Kbza220@users.noreply.github.com>
2023-10-02 04:45:14 -0300

    CardProduct

--- client/src/components/Cards/ProductCard.jsx
+++ client/src/components/Cards/ProductCard.jsx
@@ -11,11 +11,13 @@ export default  ProductCard =(props) =>{
                  className="relative w-auto h-auto  object-cover"/>
 
              </div>
-             <div className="text-stone-900 text-sm font-semibold font-jakarta-sans leading-[21px] tracking-normal">
-                {props.name}</div>
+            <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]" >
+                <div className="text-stone-900 text-sm font-semibold font-jakarta-sans leading-[21px] tracking-normal">
+                    {props.name}</div>
 
-             <div className="text-stone-900 text-sm font-semibold font-jakarta-sans leading-[21px] tracking-normal" >
-                {props.price}</div>
+                <div className="text-stone-900 text-sm font-semibold font-jakarta-sans leading-[21px] tracking-normal" >
+                    {props.price}</div>
+            </div>  
         </div>
     )
 };
