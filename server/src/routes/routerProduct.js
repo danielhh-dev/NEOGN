400373cd4889faba9ad51ab543565f1f2d32ddea
Daniel <danielhh.dev@gmail.com>
2023-10-01 20:49:56 -0600

    FilterProduct connected

--- server/src/routes/routerProduct.js
+++ server/src/routes/routerProduct.js
@@ -7,6 +7,7 @@ const { handlerGetById } = require("../controllers/handlers/handlerGetByID");
 const {
   handlersCreateProducts,
 } = require("../controllers/handlers/handlersCreateProducts");
+const getFilteredProducts = require("../controllers/Products/getProductFilter");
 const products = require("../../api/db.json");
 
 router.get("/:id", handlerGetById);
@@ -14,6 +15,8 @@ router.get("/:id", handlerGetById);
 router.get("/", (req, res) => {
   res.json(products);
 });
+router.get("/filter", getFilteredProducts);
+
 router.post("/create", handlersCreateProducts);
 
 module.exports = router;
