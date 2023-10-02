d9c83a7fcb817f1d4c6cfe4bf6ba6e9b1e9ad52e
MauricioVillanueva <mauriciogonzalovillanueva@hotmail.com>
2023-10-02 11:57:07 -0300

    imágenes cargadas en TopCategories

--- client/src/components/Home/TopCategories.jsx
+++ client/src/components/Home/TopCategories.jsx
@@ -1,20 +1,25 @@
+import Monitors from "../../utils/images/CategoriesIcons/Monitors.png"
+import Headsets from "../../utils/images/CategoriesIcons/Headsets.png"
+import Keyboards from "../../utils/images/CategoriesIcons/Keyboards.png"
+import Mice from "../../utils/images/CategoriesIcons/Mice.png"
+
 const TopCategories = () => {
     return (
         <div className="w-auto flex justify-around items-center">
             <div className="w-[76px] h-[60px] rounded-xl border border-violet-200 flex flex-col justify-center items-center">
-                <img src="https://i.postimg.cc/xjXTwHc6/monitor-9678589-7924228.png" className="w-[30px] h-[30px]"/>
+                <img src={Monitors} className="w-[30px] h-[30px]"/>
                 <div className="font-jakarta-sans font-semibold text-stone-900 text-[10px]">Monitors</div>
             </div>
             <div className="w-[76px] h-[60px] rounded-xl border border-violet-200 flex flex-col justify-center items-center">
-                <img src="https://i.postimg.cc/xdsn7TYr/gaming-headset-7480997-6138641.png" className="w-[30px] h-[30px]"/>
+                <img src={Headsets} className="w-[30px] h-[30px]"/>
                 <div className="font-jakarta-sans font-semibold text-stone-900 text-[10px]">Headsets</div>
             </div>
             <div className="w-[76px] h-[60px] rounded-xl border border-violet-200 flex flex-col justify-center items-center">
-                <img src="https://i.postimg.cc/DysfZTQs/keyboard-gaming-6013628-4979944.png" className="w-[30px] h-[30px]"/>
+                <img src={Keyboards} className="w-[30px] h-[30px]"/>
                 <div className="font-jakarta-sans font-semibold text-stone-900 text-[10px]">Keyboards</div>
             </div>
             <div className="w-[76px] h-[60px] rounded-xl border border-violet-200 flex flex-col justify-center items-center">
-                <img src="https://i.postimg.cc/1Rb5stFs/gaming-mouse-5756086-4818641.png" className="w-[30px] h-[30px]"/>
+                <img src={Mice} className="w-[30px] h-[30px]"/>
                 <div className="font-jakarta-sans font-semibold text-stone-900 text-[10px]">Mice</div>
             </div>
         </div>
