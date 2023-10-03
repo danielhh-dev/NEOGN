8281d77d49c524f32b667348f0f0279ba7a525f8
Alan <alanjesuseliasruiz@gmail.com>
2023-10-03 11:42:51 -0300

    wait

--- server/src/routes/routerProduct.js
+++ server/src/routes/routerProduct.js
@@ -1,18 +1,27 @@
 const { Router } = require("express");
 const router = Router();
-const { handlerGetProducts} = require("../controllers/handlers/handlerGetProducts");
-const {handlerGetById} = require("../controllers/handlers/handlerGetByID");
-const {handlersCreateProducts} = require("../controllers/handlers/handlersCreateProducts");
-const {handlerDeleteById} = require("../controllers/handlers/handlerDeleteById");
-const {handlerFilterProducts} = require('../controllers/handlers/handlerFilterProducts');
-const products = require("../../api/db.json");
+const {
+  handlerGetProducts,
+} = require("../controllers/handlers/handlerGetProducts");
+const { handlerGetById } = require("../controllers/handlers/handlerGetByID");
+const {
+  handlersCreateProducts,
+} = require("../controllers/handlers/handlersCreateProducts");
+const {
+  handlerDeleteById,
+} = require("../controllers/handlers/handlerDeleteById");
+const {
+  handlerFilterProducts,
+} = require("../controllers/handlers/handlerFilterProducts");
+// const products = require("../../api/db.json");
 
-router.get("/:id", handlerGetById);
 // router.get("/", handlerGetProducts);
-router.get("/", (req, res) => {
-  res.json(products);
-});
+// router.get("/", (req, res) => {
+//   res.json(products);
+// });
 router.get("/filter", handlerFilterProducts);
+router.get("/", handlerGetProducts);
+router.get("/:id", handlerGetById);
 
 router.post("/create", handlersCreateProducts);
 router.delete("/:id", handlerDeleteById);
