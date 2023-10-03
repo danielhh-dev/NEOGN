f753f300faf0f225ce96593000c4c9f55c12cc95
jkarenas <jc.arenas1993@gmail.com>
2023-10-03 16:55:24 -0500

    tailwind de los filtros

--- client/src/views/Search.jsx
+++ client/src/views/Search.jsx
@@ -1,7 +1,11 @@
+import FilterSortRange from "../components/FilterSortRange";
+
 const Search = () => {
     return(
         <div>
             Search
+            {/*falta boton para renderizar FilterSortRange */}
+            <FilterSortRange/>
         </div>
     )
 }
