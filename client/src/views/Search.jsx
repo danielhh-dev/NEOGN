bfd7c6e80c1b31b4c962d977cda682bad5d3198c
Neider690 <neider.polo.090600@gmail.com>
2023-10-04 16:56:46 -0500

    se incluye el componente filter en la view search

--- client/src/views/Search.jsx
+++ client/src/views/Search.jsx
@@ -1,54 +1,55 @@
-import { useEffect } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import fetchProducts from "../redux/actions/getProducts";
-import HomeCard from "../components/Cards/HomeCard";
 import TopCategories from "../components/Home/TopCategories";
 import SearchCard from "../components/Cards/SearchCard";
-
-
-
+import { LuSettings2 } from "react-icons/lu";
 import FilterSortRange from "../components/FilterSortRange";
 
 const Search = () => {
-
   const dispatch = useDispatch();
-  const products = useSelector((state)=> state.products)
+  const products = useSelector((state) => state.products);
+  const [showFilter, setShowFilter] = useState(false);
+
+  const toFilter = () => {
+    setShowFilter(!showFilter); // Alternar la visibilidad del componente FilterSortRange
+  };
+
   useEffect(() => {
     dispatch(fetchProducts());
   }, [dispatch]);
-
-
-
 
   return (
     <div className="h-full pb-32">
       <div className="font-jakarta-sans w-auto flex justify-between items-center mx-10 my-6">
         <h1 className="text-stone-900 text-[18px] font-bold tracking-wide">
-          By Category 
+          By Category
         </h1>
-        <p className="text-red-500 text-[10px] font-semibold">FILTERS</p>
+        <button onClick={toFilter}>
+          <LuSettings2 className="text-black-500 text-[30px] font-semibold" />
+        </button>
       </div>
+      {showFilter && <FilterSortRange />} {/* Renderizar FilterSortRange si showFilter es true */}
       <div className="w-auto h-auto m-6">
         <TopCategories />
       </div>
       <div className="font-jakarta-sans w-auto flex justify-between items-center mx-10 my-6">
         <h1 className="text-stone-900 text-[18px] font-bold tracking-wide">
-          Produts
+          Products
         </h1>
       </div>
       <div className="w-full flex justify-center items-center">
         <div className="w-auto h-auto grid grid-cols-1 gap-4">
-        {products.products.map((product) => (
-      <SearchCard  
-        key={product.id}
-        id={product.id}
-        name={product.name}
-        price={product.price} 
-        image={product.image}
-        description={product.description}
-        
-        />
-    ))}
+          {products.products.map((product) => (
+            <SearchCard
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              image={product.image}
+              description={product.description}
+            />
+          ))}
         </div>
       </div>
     </div>
