e88edca3fe12912df29cb76c67cb2027ee9db67a
Maximiliano Anguita <maximilianoanguita@MacBook-Pro-de-Maximiliano.local>
2023-10-04 21:42:50 -0300

    Mejora en la funcionalidad de búsqueda y la interfaz en el componente TopBar

--- client/src/components/TopBar/TopBar.jsx
+++ client/src/components/TopBar/TopBar.jsx
@@ -1,13 +1,24 @@
-import Searchbar from "./SearchBar"
-import Sidebar from "./Sidebar"
+import Searchbar from "./SearchBar";
+import Sidebar from "./Sidebar";
+import ActiveHeartImage from "../../utils/images/AppbarIcons/Neogn.jpeg";
+import { useState } from "react";
 
 const TopBar = () => {
+  const [inputVisible, setInputVisible] = useState(false);
+
+  const toggleInput = () => setInputVisible(!inputVisible)
+
     return (
-        <div className="w-full h-auto flex items-center ">
-            <Sidebar/>
-            <Searchbar/>
+        <div className="w-full flex flex-col">
+            <div className="w-full h-auto flex items-center justify-between ">
+                <Sidebar />
+                <img src={ActiveHeartImage} alt="Active Heart" className="h-14 w-auto lg:hidden mr-4" />
+                <div className="lg:flex items-center"></div>
+                <Searchbar classNameContainer={inputVisible ? "sm:bg-zinc-100 justify-end sm:justify-between" : "justify-end"} toggleInput={toggleInput} classNameBtn={"block"} classNameInput={inputVisible ? "hidden sm:block w-full" : "hidden"}/>
+            </div>
+            <Searchbar className="block sm:hidden" classNameContainer={inputVisible && "bg-zinc-100 justify-end"} toggleInput={toggleInput} classNameBtn={"hidden"} classNameInput={inputVisible ? "block sm:hidden w-full" : "hidden"}/>
         </div>
-    )
-}
+    );
+};
 
 export default TopBar;
